92b465d7527d5ae2d96c19090f749702c9e1e49d
Masanao KOEDA <mkoeda@gmail.com>
2021-02-03 14:43:32 +0900

    create folders

--- 1/common.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks import ev3brick as brick
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
-from pybricks.parameters import Port, Direction, Stop, Color, Button, Align, ImageFile, SoundFile
-from pybricks.tools import wait, StopWatch
-from pybricks.robotics import DriveBase
-
-# ここからプログラムを書きましょう
