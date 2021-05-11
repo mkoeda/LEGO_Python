f254a1b32b06fda9b02ee8bd9626c33a443267af
Masanao KOEDA <mkoeda@gmail.com>
2021-05-11 18:45:35 +0900

    Update 6-1.py

--- sampleprogram/MicroPython1/6/6-1.py
+++ sampleprogram/MicroPython1/6/6-1.py
@@ -7,5 +7,5 @@ geared_motor = Motor(Port.A, Direction.CLOCKWISE, [36, 12])
 #駆動歯車数 12, 従動歯車数 36の場合
 #geared_motor = Motor(Port.A, Direction.CLOCKWISE, [12, 36])
 
-#回転速度 100 deg/s で， 90 度回転させる
+#回転速度 100 deg/s で， 90 度回転させる
 geared_motor.run_angle(100,90)
