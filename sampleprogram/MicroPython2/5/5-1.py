cd7f73ca12a7428c8a677aa9aa26c112e3eb39ce
ntakayuk <76199197+ntakayuk@users.noreply.github.com>
2021-11-23 13:45:20 +0900

    update

--- sampleprogram/MicroPython2/5/5-1.py
+++ sampleprogram/MicroPython2/5/5-1.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env pybricks-micropython
 from common import *
 
-# Create your objects here.
 ev3 = EV3Brick()
 
-# Write your program here
 led_color = [Color.GREEN, Color.ORANGE]
 touch_sensor = TouchSensor(Port.S1)
 
-while True: #
+while True:
     ev3.light.on(led_color[int(touch_sensor.pressed())])
-    # waiting for 10 milliseconds
     wait(10)
